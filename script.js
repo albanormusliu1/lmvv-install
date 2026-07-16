7575c0387287438b0c7dbbdd7603bb3e5bddf828
albanormusliu1 <albanormuslija43@gmail.com>
2026-07-16 19:35:43 +0200

    Implement user authentication with localStorage
    Përditësimi i login-it me kontrollin e përdoruesve dhe roleve.

--- script.js
+++ script.js
@@ -3,7 +3,21 @@ function login() {
     let username = document.getElementById("username").value;
     let password = document.getElementById("password").value;
 
-    if (username === "admin" && password === "1234") {
+
+    let perdoruesit = JSON.parse(localStorage.getItem("perdoruesit")) || [];
+
+
+    let perdoruesi = perdoruesit.find(
+        user => user.username === username && user.password === password
+    );
+
+
+    if (perdoruesi) {
+
+        localStorage.setItem(
+            "perdoruesAktiv",
+            JSON.stringify(perdoruesi)
+        );
 
         window.location.href = "dashboard.html";
 
