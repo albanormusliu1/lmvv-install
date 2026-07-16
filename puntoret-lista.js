2b1f8223bf610c07ce823ddc07a124f422fefa4c
albanormusliu1 <albanormuslija43@gmail.com>
2026-07-16 21:00:03 +0200

    Update puntoret-lista.js

--- puntoret-lista.js
+++ puntoret-lista.js
@@ -3,15 +3,56 @@ let puntoret = JSON.parse(localStorage.getItem("puntoret")) || [];
 let tabela = document.getElementById("listaPuntoreve");
 
 
-puntoret.forEach((puntor, index) => {
 
-    tabela.innerHTML += `
-    <tr>
-        <td>${index + 1}</td>
-        <td>${puntor.emri}</td>
-        <td>${puntor.telefoni}</td>
-        <td>${puntor.pozita}</td>
-    </tr>
-    `;
+puntoret.forEach((puntor,index)=>{
+
+
+tabela.innerHTML += `
+
+<tr>
+
+<td>${index + 1}</td>
+
+<td>${puntor.emri}</td>
+
+<td>${puntor.telefoni}</td>
+
+<td>${puntor.pozita}</td>
+
+
+<td>
+
+<button onclick="editoPuntor(${index})">
+
+✏️ Edit
+
+</button>
+
+</td>
+
+
+</tr>
+
+`;
 
 });
+
+
+
+
+function editoPuntor(index){
+
+
+localStorage.setItem(
+
+"puntorEdit",
+
+index
+
+);
+
+
+window.location.href="edit-punetor.html";
+
+
+}
