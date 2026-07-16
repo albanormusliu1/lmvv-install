5b43c1c13cc8f740861ec565ff21144340a70510
albanormusliu1 <albanormuslija43@gmail.com>
2026-07-16 19:32:53 +0200

    Add user data to perdoruesit.js
    Krijimi i sistemit bazë të përdoruesve dhe roleve.

---
+++ perdoruesit.js
@@ -0,0 +1,17 @@
+const perdoruesit = [
+    {
+        username: "admin",
+        password: "1234",
+        roli: "admin"
+    },
+    {
+        username: "punetor1",
+        password: "1234",
+        roli: "punetor"
+    }
+];
+
+localStorage.setItem(
+    "perdoruesit",
+    JSON.stringify(perdoruesit)
+);
