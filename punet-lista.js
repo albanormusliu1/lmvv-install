710eeebb60edc2aa9b59c6b7286b2c8665d0b5f4
albanormusliu1 <albanormuslija43@gmail.com>
2026-07-16 21:03:16 +0200

    Update punet-lista.js

--- punet-lista.js
+++ punet-lista.js
@@ -1,18 +1,61 @@
 let punet = JSON.parse(localStorage.getItem("punet")) || [];
 
+
 let tabela = document.getElementById("listaPuneve");
 
 
-punet.forEach((pune, index) => {
 
-    tabela.innerHTML += `
-    <tr>
-        <td>${index + 1}</td>
-        <td>${pune.klienti}</td>
-        <td>${pune.punetori}</td>
-        <td>${pune.lloji}</td>
-        <td>${pune.statusi}</td>
-    </tr>
-    `;
+punet.forEach((pune,index)=>{
+
+
+tabela.innerHTML += `
+
+<tr>
+
+<td>${index + 1}</td>
+
+<td>${pune.klienti}</td>
+
+<td>${pune.puntori}</td>
+
+<td>${pune.lloji}</td>
+
+<td>${pune.statusi}</td>
+
+
+<td>
+
+<button onclick="editoPune(${index})">
+
+✏️ Edit
+
+</button>
+
+</td>
+
+
+</tr>
+
+`;
 
 });
+
+
+
+
+function editoPune(index){
+
+
+localStorage.setItem(
+
+"puneEdit",
+
+index
+
+);
+
+
+window.location.href="edit-pune.html";
+
+
+}
