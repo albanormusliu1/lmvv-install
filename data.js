ecaba8431eb93057ccc61dd0d69d1366f6a99660
albanormusliu1 <albanormuslija43@gmail.com>
2026-07-16 18:18:25 +0200

    Create data.js
    Krijimi i të dhënave fillestare për punëtorët dhe klientët.

---
+++ data.js
@@ -0,0 +1,27 @@
+const punetoret = [
+    {
+        id: 1,
+        emri: "Punëtori 1",
+        pozita: "Instalues ngrohjeje"
+    },
+    {
+        id: 2,
+        emri: "Punëtori 2",
+        pozita: "Teknik"
+    },
+    {
+        id: 3,
+        emri: "Punëtori 3",
+        pozita: "Ndihmës"
+    }
+];
+
+
+const klientet = [
+    {
+        id: 1,
+        emri: "Klienti test",
+        telefoni: "000000000",
+        adresa: "Adresa test"
+    }
+];
